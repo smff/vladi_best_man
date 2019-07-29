3985c0912f8f0f902265f98cb60198d8d8a7ca67
Смирнов Дмитрий Алексеевич <Smirnov.D.Aleks@sberbank.ru>
2019-07-29 17:34:12 +0300

    yo

---
+++ src/components/second/Second.ts
@@ -0,0 +1,8 @@
+import {First} from "../first/First";
+
+export class Second {
+
+    private wow(){
+        return new First();
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import {First} from "./components/first/First";
+import {Second} from "./components/second/Second";
+
+export {
+    First,
+    Second
+}
